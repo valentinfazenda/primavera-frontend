f4badb5e915194fbce21e8a09dcb211536294602
Valentin Fazenda <valentin.fazenda@hotmail.com>
2025-08-29 10:46:44 +0200

    Add dynamic and revalidation exports to SignIn and WaitingList pages

--- src/app/(auth)/waiting-list/page.tsx
+++ src/app/(auth)/waiting-list/page.tsx
@@ -1,6 +1,9 @@
 import WaitingListPageInner from "@/components/waiting-list/WaitingListPageInner";
 import { Suspense } from "react";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function page() {
   return (
     <Suspense fallback={<div>Chargement...</div>}>
